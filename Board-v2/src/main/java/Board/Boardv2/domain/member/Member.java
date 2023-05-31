70ca677075deaf3c92bb2bf9fe669b72f902bd41
Jihyun3478 <leejihyun3478@gmail.com>
2023-05-31 13:29:26 +0900

    update 2023.05.31

--- Board-v2/src/main/java/Board/Boardv2/domain/member/Member.java
+++ Board-v2/src/main/java/Board/Boardv2/domain/member/Member.java
@@ -3,19 +3,24 @@ package Board.Boardv2.domain.member;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
+import java.util.ArrayList;
 
 @Entity
+@Table(name = "member")
 @Getter @Setter
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String loginID;
     private String loginPW;
     private String name;
     private String email;
+
+//    @OneToMany(mappedBy = "member")
+//    private List<Post> posts = new ArrayList<>();
 }
--- Board-v2/src/main/java/Board/Boardv2/domain/post/Post.java
+++ Board-v2/src/main/java/Board/Boardv2/domain/post/Post.java
@@ -1,25 +1,25 @@
-//package Board.Boardv2.domain.posts;
-//
-//import lombok.Data;
-//
-//@Data
-//public class Post {
-//
-//    private Long postId;
-//    private String postTitle;
-//    private String postWriter;
-//    private String postContent;
-//    private String postDate;
-//    private int postHit;
-//
-//    public Post() {}
-//
-//    public Post(String postTitle, String postWriter, String postContent, String postDate, int postHit) {
-//
-//        this.postTitle = postTitle;
-//        this.postWriter = postWriter;
-//        this.postContent = postContent;
-//        this.postDate = postDate;
-//        this.postHit = postHit;
-//    }
-//}
+package Board.Boardv2.domain.post;
+
+import lombok.Data;
+
+@Data
+public class Post {
+
+    private Long postId;
+    private String postTitle;
+    private String postWriter;
+    private String postContent;
+    private String postDate;
+    private int postHit;
+
+    public Post() {}
+
+    public Post(String postTitle, String postWriter, String postContent, String postDate, int postHit) {
+
+        this.postTitle = postTitle;
+        this.postWriter = postWriter;
+        this.postContent = postContent;
+        this.postDate = postDate;
+        this.postHit = postHit;
+    }
+}
