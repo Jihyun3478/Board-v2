e975c2988dd22a168a946f128bccaaac8aa0d930
Jihyun3478 <leejihyun3478@gmail.com>
2023-05-31 22:14:44 +0900

    update 2023.05.31

--- Board-v2/src/main/java/Board/Boardv2/service/MemberService.java
+++ Board-v2/src/main/java/Board/Boardv2/service/MemberService.java
@@ -1,4 +1,48 @@
 package Board.Boardv2.service;
 
+import Board.Boardv2.domain.Member;
+import Board.Boardv2.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
 public class MemberService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    /*
+     * 회원가입
+     */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicationMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicationMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /*
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /*
+     * 개인 조회
+     */
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
 }
