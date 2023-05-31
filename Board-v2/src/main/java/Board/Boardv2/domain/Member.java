c17a2167276ccc31b33940e5d91a523fc030f132
Jihyun3478 <leejihyun3478@gmail.com>
2023-05-31 22:05:09 +0900

    update 2023.05.31

--- Board-v2/src/main/java/Board/Boardv2/domain/Member.java
+++ Board-v2/src/main/java/Board/Boardv2/domain/Member.java
@@ -1,12 +1,11 @@
 package Board.Boardv2.domain;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "member")
