2758135067c56c0d151857d6adc9f382b901f061
Jihyun3478 <leejihyun3478@gmail.com>
2023-06-02 20:30:47 +0900

    update 2023.06.02

--- Board-v2/src/main/java/Board/Boardv2/controller/HomeController.java
+++ Board-v2/src/main/java/Board/Boardv2/controller/HomeController.java
@@ -1,16 +1,31 @@
 package Board.Boardv2.controller;
 
+import Board.Boardv2.domain.Member;
+import Board.Boardv2.session.SessionConst;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
-@Controller
 @Slf4j
-public class HomeController {
+@Controller
+@RequiredArgsConstructor
 
-    @RequestMapping("/")
-    public String home() {
-        log.info("home controller");
-        return "home";
+public class HomeController {
+    @GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
+        // 세션에 회원 데이터가 없으면 home으로
+        if(loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
