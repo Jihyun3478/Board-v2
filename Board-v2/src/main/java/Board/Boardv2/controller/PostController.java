97f53c9468ee9a0f1716762515d13d736f344e38
Jihyun3478 <leejihyun3478@gmail.com>
2023-06-05 21:51:26 +0900

    update 2023.06.05

--- Board-v2/src/main/java/Board/Boardv2/controller/PostController.java
+++ Board-v2/src/main/java/Board/Boardv2/controller/PostController.java
@@ -1,18 +1,19 @@
 package Board.Boardv2.controller;
 
 
+import Board.Boardv2.domain.Member;
 import Board.Boardv2.domain.Post;
-import Board.Boardv2.repository.PostRepository;
 import Board.Boardv2.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.annotation.PostConstruct;
+import javax.validation.Valid;
 import java.util.Date;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,5 +21,25 @@ public class PostController {
 
     private final PostService postService;
 
-    
+    @GetMapping("/posts/new")
+    public String createForm(Model model) {
+        model.addAttribute("post", new Post());
+        return "post/addPost";
+    }
+
+    @PostMapping("/posts/new")
+    public String create(@Valid @ModelAttribute("post") Post post, BindingResult result) {
+        if(result.hasErrors()) {
+            return "post/addPost";
+        }
+
+        Post addPost = new Post();
+        Member member = new Member();
+        Date date = new Date();
+        addPost.setPostTitle(post.getPostTitle());
+        addPost.setPostWriter(member.getName());
+        addPost.setPostContent(post.getPostContent());
+        addPost.setPostDate();
+        addPost.setPostTitle(post.getPostTitle());
+    }
 }
--- Board-v2/src/main/java/Board/Boardv2/domain/Post.java
+++ Board-v2/src/main/java/Board/Boardv2/domain/Post.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 //@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -16,13 +17,18 @@ public class Post {
     @Column(name = "post_id")
     private Long postId;
 
+    @NotEmpty(message = "제목을 입력해주세요.")
     private String postTitle;
+
     private String postWriter;
+
+    @NotEmpty(message = "내용을 입력해주세요.")
     private String postContent;
+
     private String postDate;
     private int postHit;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    private Member member;
 }
